cc77041f742be2f6848164c2d48590a6cf93a6bd
Anjani Kumar Pathak <anjani.pathak@netcracker.com>
2026-07-15 00:35:44 +0530

    feat[cache]: add cache controller

--- src/main/java/com/rag/RagApplication.java
+++ src/main/java/com/rag/RagApplication.java
@@ -2,8 +2,10 @@ package com.rag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // required by NgrokWebhookScheduler, which re-checks the ngrok URL periodically
 public class RagApplication {
     public static void main(String[] args) {
         SpringApplication.run(RagApplication.class, args);
