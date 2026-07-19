f4a6a07a1149a9be854db6824dcc7b77d7c1ff1a
Anjani Kumar Pathak <anjani.pathak@netcracker.com>
2026-07-19 15:31:16 +0530

    feat[upload]: update config to support bigger file size

--- src/main/java/com/rag/config/PoiConfig.java
+++ src/main/java/com/rag/config/PoiConfig.java
@@ -2,12 +2,14 @@ package com.rag.config;
 
 import jakarta.annotation.PostConstruct;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.util.IOUtils;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PoiConfig {
     @PostConstruct
     public void configPoi() {
+        IOUtils.setByteArrayMaxOverride(200_000_000);
         ZipSecureFile.setMinInflateRatio(0.001);
         ZipSecureFile.setMaxEntrySize(500L * 1024 * 1024);
         ZipSecureFile.setMaxTextSize(200L * 1024 * 1024);
